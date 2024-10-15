d605b6e0dc720a8dc732f1f86875d1e8cb7c29c5
서정원 <125368624+Seo-garden@users.noreply.github.com>
2024-10-15 16:05:23 +0900

    [Silver IV] Title: 보물, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.swift
+++ 백준/Silver/1026. 보물/보물.swift
@@ -3,15 +3,11 @@ let N = Int(readLine()!)!
 var A = [Int]()
 var B = [Int]()
 
-let inputA = readLine()!.split(separator: " ").compactMap { Int($0)! }
-let inputB = readLine()!.split(separator: " ").compactMap { Int($0)! }
-A.insert(contentsOf: inputA, at: 0)
-B.insert(contentsOf: inputB, at: 0)
+let inputA = readLine()!.split(separator: " ").compactMap { Int($0)! }.sorted(by: >)
+let inputB = readLine()!.split(separator: " ").compactMap { Int($0)! }.sorted()
 var result = 0
-var inputAsort = inputA.sorted(by: <)
-var inputBsort = inputB.sorted(by: >)
 
 for i in 0..<N {
-    result += inputAsort[i] * inputBsort[i]
+    result += inputA[i] * inputB[i]
 }
 print(result)
