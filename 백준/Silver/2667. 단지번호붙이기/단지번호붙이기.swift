e9afbd4533e00e469b1b2011fa39950462d200eb
서정원 <125368624+Seo-garden@users.noreply.github.com>
2025-03-19 10:27:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
@@ -1,39 +1,35 @@
-//백준 2667
-import Foundation
+let n = Int(readLine()!)!
+var graph = [[Int]]()
 
-let n = Int(readLine()!)! // 입력되는 한 변의 크기
-var map = [[Int]]()
 var count = 0
-var block = [Int]()
+var home = [Int]()
 
 for _ in 0..<n {
-    map.append(Array(readLine()!.map({Int(String($0))!})))
+    let input = Array(readLine()!.map{ Int(String($0))! })
+    graph.append(input)
 }
 
-// dfs 함수
-func dfs(_ x: Int, _ y: Int) {
-    if x < 0 || y < 0 || x >= n || y >= n || map[x][y] == 0 { return } // 예외
+private func DFS(_ x: Int, _ y: Int) {
+    if x < 0 || y < 0 || x >= n || y >= n || graph[x][y] == 0 { return }
     
-    count += 1 // 단지 넓이 누적
-    map[x][y] = 0 // 이미 체크한 자리는 0으로 바꿔서 중복 검사 피해주기
+    count += 1
+    graph[x][y] = 0     //방문 처리
     
-    // 재귀 호출
-    dfs(x+1, y)
-    dfs(x-1, y)
-    dfs(x, y+1)
-    dfs(x, y-1)
+    DFS(x, y+1)
+    DFS(x, y-1)
+    DFS(x+1, y)
+    DFS(x-1, y)
 }
 
-// 메인 실행
 for i in 0..<n {
     for j in 0..<n {
-        if map[i][j] == 1 {
-            count = 0 // count 초기화
-            dfs(i, j) // dfs 호출
-            block.append(count) // block 배열에 방금 구한 단지 크기 추가
+        if graph[i][j] == 1 {
+            count = 0
+            DFS(i, j)
+            home.append(count)
         }
     }
 }
 
-print(block.count)
-print(block.sorted().map({String($0)}).joined(separator: "\n"))
+print(home.count)
+print(home.sorted().map({ String($0)}).joined(separator: "\n"))
