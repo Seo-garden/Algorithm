45a15f821951ac278b0ba89dce43aa8d8c06e849
서정원 <125368624+Seo-garden@users.noreply.github.com>
2025-04-07 21:23:35 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
@@ -1,10 +1,12 @@
-var nums: Set<Int> = []
+var num = Set<Int>()
+
 for _ in 1...28 {
-    nums.insert(Int(readLine()!)!)
+    let input = Int(readLine()!)!
+    num.insert(input)
 }
 
 for i in 1...30 {
-    if !nums.contains(i) {
+    if !num.contains(i) {
         print(i)
     }
 }
