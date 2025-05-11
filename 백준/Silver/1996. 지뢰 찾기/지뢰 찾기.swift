57a8f4280d7fe6a8fb29549383f78b2439652931
서정원 <125368624+Seo-garden@users.noreply.github.com>
2025-05-11 14:43:47 +0900

    [Silver V] Title: 지뢰 찾기, Time: 184 ms, Memory: 92644 KB -BaekjoonHub

--- 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.swift
+++ 백준/Silver/1996. 지뢰 찾기/지뢰 찾기.swift
@@ -1,26 +1,25 @@
 let n = Int(readLine()!)!
 var map = [[Character]]()
-
-let dx = [0, 0, -1, 1, 1, -1, 1, -1]
-let dy = [1, -1, 0, 0, 1, -1, -1, 1]
-
 var result = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+let dx = [-1, 0, 1, 1, 1, 0, -1, -1]
+let dy = [1, 1, 1, 0, -1, -1, -1, 0]
 
 for _ in 0..<n {
     let input = Array(readLine()!)
     map.append(input)
 }
 
-for i in 0..<n {
-    for j in 0..<n {
-        if map[i][j] != "." {
-            let power = Int(String(map[i][j]))!
+for x in 0..<n {
+    for y in 0..<n {
+        if map[x][y] != "." {       //숫자가 존재
+            let power = Int(String(map[x][y]))!
             
-            for k in 0..<8 {
-                let nx = dx[k] + i
-                let ny = dy[k] + j
+            for k in 0..<8 {                //8방향 탐색
+                let nx = dx[k] + x
+                let ny = dy[k] + y
                 
-                if nx >= 0 && nx < n && ny >= 0 && ny < n {
+                if nx >= 0 && nx < n && ny >= 0 && ny < n {     //맵 밖으로 탐색하지 않기 위함
                     if map[nx][ny] == "." {
                         result[nx][ny] += power
                     }
@@ -32,12 +31,15 @@ for i in 0..<n {
 
 for i in 0..<n {
     var line = ""
+    
     for j in 0..<n {
         if map[i][j] == "." {
             line += result[i][j] >= 10 ? "M" : String(result[i][j])
         } else if map[i][j] != "." {
             line += "*"
-        } else { line = "*" }
+        } else {
+            line = "*"
+        }
     }
     print(line)
 }
