1510778371d4954b91bb0e3d00bcbc7c4dac2733
서정원 <125368624+Seo-garden@users.noreply.github.com>
2025-06-04 19:43:35 +0900

    [Silver III] Title: 거북이, Time: 184 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/8911. 거북이/거북이.swift
@@ -0,0 +1,42 @@
+let t = Int(readLine()!)!
+
+//동북서남
+let dx = [1, 0, -1, 0]
+let dy = [0, -1, 0, 1]
+
+for _ in 0..<t {
+    var x = 0
+    var y = 0
+    var dir = 0
+    var path = [(x: Int, y: Int)]()
+    path.append((x,y))
+    
+    let input = readLine()!
+    
+    for i in input {
+        switch i {
+        case "F":
+            x += dx[dir]
+            y += dy[dir]
+            
+        case "B":
+            x -= dx[dir]
+            y -= dy[dir]
+            
+        case "L":
+            dir = (dir + 1) % 4
+        case "R":
+            dir = (dir + 3) % 4
+        default:
+            break
+        }
+        path.append((x,y))
+    }
+    
+    let minX = path.map { $0.x }.min()!
+    let maxX = path.map { $0.x }.max()!
+    let minY = path.map { $0.y }.min()!
+    let maxY = path.map { $0.y }.max()!
+    
+    print((maxX - minX) * (maxY - minY))
+}
